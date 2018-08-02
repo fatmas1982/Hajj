9e34f29b7c307bff8fcc40777b62ed0ea091b5fa
Fatma.s.gadelrab <f_s_g2001@yahoo.co.uk>
2018-08-02 02:12:17 +0200

    M-09-HajjFood
    Beta version

---
+++ config.ini.php
@@ -0,0 +1,9 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+[database]
+host                 = localhost
+username             = 
+password             = root1
+dbname               = id6663956_categories_db
+port                 = 3306
+adapter              = PDO_MYSQL ; PDO_MYSQL or MYSQLI
+
